26d752d44d29c0c2c89fc0d16db63a532b174171
Leonardo Alves <85313751+LeonardAlves@users.noreply.github.com>
2024-12-26 16:32:27 +0000

    Add files via upload

---
+++ teste1.js
@@ -0,0 +1,8 @@
+let contador = 1;
+
+while(contador <= 10) {
+    console.log(`Contagem ${contador}`);
+    alert(contador);
+    contador++;
+    
+}
---
+++ teste3.js
@@ -0,0 +1,8 @@
+let contador = prompt ('Escolha um numero de 1 a 10, para contagem regressiva.')
+
+while(contador >= 0) {
+    console.log(`Contagem ${contador}`);
+    alert(contador);
+    contador--;
+    
+}
